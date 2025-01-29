561a1cae29b5a7f2899b625aa5cb5ac79875389f
anazks <anazksunil2@gmail.com>
2025-01-29 12:53:43 +0530

    pwa added

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 import { BrowserRouter } from "react-router-dom";
+import { registerSW } from "virtual:pwa-register";  // Import the service worker register function
 
+// Register the service worker for PWA functionality
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New version available. Reload?")) {
+      updateSW(true);
+    }
+  },
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-        <App />
+      <App />
     </BrowserRouter>
   </StrictMode>,
-)
+);
